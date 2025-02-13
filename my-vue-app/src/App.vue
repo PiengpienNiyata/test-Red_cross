8ff798753d45e6029f54e28a4b9a76b94537f8ba
Piengpien Niyata <piengpien.nyt@gmail.com>
2025-02-13 12:02:01 +0700

    header

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -1,11 +1,22 @@
 <template>
   <div>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <Header />
+    <router-view />
+  </div>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container">
         <router-link to="/" class="navbar-brand">Home</router-link>
         <!-- <router-link to="/questionnairesResearcher" class="nav-link">Questionnaire</router-link> -->
       </div>
     </nav>
-    <router-view />
-  </div>
 </template>
+
+<script>
+import Header from "@/components/Header.vue";
+
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
---
+++ my-vue-app/src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <header class="header d-flex align-items-center">
+      <div class="logo_bg d-flex align-items-center justify-content-center">
+        <img src="@/assets/LOGO_red_cross.png" alt="Logo" class="logo" />
+      </div>
+    </header>
+  </template>
+  
+  <style scoped>
+  .header {
+    width: 1366px;
+    height: 120px;
+    background: linear-gradient(to right, #EB4648, #F28A8B, #F49899, #FFFFFF);
+    display: flex;
+  }
+  
+  .logo_bg {
+    background-color: #FFFFFF;
+    width: 230px;
+    height: 100%;
+    padding: 16px 0;
+  }
+  
+  .logo {
+    width: 139px;
+    height: 94px;
+  }
+  </style>
+  
