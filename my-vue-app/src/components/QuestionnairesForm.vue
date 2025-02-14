c4555dcfea43586ec311f898acf22b3d95d95230
Piengpien Niyata <piengpien.nyt@gmail.com>
2025-02-14 09:38:49 +0700

    Update QuestionnairesForm.vue

--- my-vue-app/src/components/QuestionnairesForm.vue
+++ my-vue-app/src/components/QuestionnairesForm.vue
@@ -52,44 +52,24 @@ const saveResponses = () => {
   <div class="container mt-4">
     <h3 v-if="questionnaire.title !== 'null'">{{ questionnaire.title }}</h3>
     <form @submit.prevent="saveResponses">
-      <div
-        v-for="section in questionnaire.sections"
-        :key="section.name"
-        class="mb-4"
-      >
+      <div v-for="section in questionnaire.sections" :key="section.name" class="mb-4">
         <h4 v-if="section.name !== 'null'" class="mt-3">{{ section.name }}</h4>
 
         <div v-for="q in section.questions" :key="q.id" class="mb-3">
           <label class="form-label">{{ q.question }}</label>
 
-          <input
-            v-if="q.type === 'text'"
-            v-model="answers[q.id]"
-            type="text"
-            class="form-control"
-          />
+          <input v-if="q.type === 'text'" v-model="answers[q.id]" type="text" class="form-control" />
 
           <div v-else-if="q.type === 'radio'">
             <div v-for="option in q.options" :key="option" class="form-check">
-              <input
-                class="form-check-input"
-                type="radio"
-                :name="'q' + q.id"
-                :value="option"
-                v-model="answers[q.id]"
-              />
+              <input class="form-check-input" type="radio" :name="'q' + q.id" :value="option" v-model="answers[q.id]" />
               <label class="form-check-label">{{ option }}</label>
             </div>
           </div>
 
           <div v-else-if="q.type === 'checkbox'">
             <div v-for="option in q.options" :key="option" class="form-check">
-              <input
-                class="form-check-input"
-                type="checkbox"
-                :value="option"
-                v-model="answers[q.id]"
-              />
+              <input class="form-check-input" type="checkbox" :value="option" v-model="answers[q.id]" />
               <label class="form-check-label">{{ option }}</label>
             </div>
           </div>
@@ -98,4 +78,49 @@ const saveResponses = () => {
       <button type="submit" class="btn btn-danger">Submit</button>
     </form>
   </div>
+
+  <form class="row g-3">
+    <div class="col-md-6">
+      <label for="inputEmail4" class="form-label">Email</label>
+      <input type="email" class="form-control" id="inputEmail4">
+    </div>
+    <div class="col-md-6">
+      <label for="inputPassword4" class="form-label">Password</label>
+      <input type="password" class="form-control" id="inputPassword4">
+    </div>
+    <div class="col-12">
+      <label for="inputAddress" class="form-label">Address</label>
+      <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+    </div>
+    <div class="col-12">
+      <label for="inputAddress2" class="form-label">Address 2</label>
+      <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+    </div>
+    <div class="col-md-6">
+      <label for="inputCity" class="form-label">City</label>
+      <input type="text" class="form-control" id="inputCity">
+    </div>
+    <div class="col-md-4">
+      <label for="inputState" class="form-label">State</label>
+      <select id="inputState" class="form-select">
+        <option selected>Choose...</option>
+        <option>...</option>
+      </select>
+    </div>
+    <div class="col-md-2">
+      <label for="inputZip" class="form-label">Zip</label>
+      <input type="text" class="form-control" id="inputZip">
+    </div>
+    <div class="col-12">
+      <div class="form-check">
+        <input class="form-check-input" type="checkbox" id="gridCheck">
+        <label class="form-check-label" for="gridCheck">
+          Check me out
+        </label>
+      </div>
+    </div>
+    <div class="col-12">
+      <button type="submit" class="btn btn-primary">Sign in</button>
+    </div>
+  </form>
 </template>
