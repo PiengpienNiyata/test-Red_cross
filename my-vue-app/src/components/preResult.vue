252a0b40a37e495fe0162d00a88d98d27328f077
Piengpien Niyata <piengpien.nyt@gmail.com>
2025-03-07 20:19:35 +0700

    Update preResult.vue

--- my-vue-app/src/components/preResult.vue
+++ my-vue-app/src/components/preResult.vue
@@ -9,6 +9,26 @@ const emit = defineEmits(["continue"]);
   <div class="pre-result">
     <h4>คุณเลือก "ไม่แน่ใจ" ในคำถามล่าสุด</h4>
     <p v-if="props.lastQuestion">คำถามล่าสุดที่ตอบ: {{ props.lastQuestion }}</p>
-    <button @click="emit('continue')">ดำเนินการต่อ</button>
+    <button type="button" class="btn" @click="emit('continue')">ดำเนินการต่อ</button>
   </div>
 </template>
+
+<style >
+.pre-result{
+  
+}
+.btn{
+  height: 100%;
+  width: 144px;
+  background-color: #EB4648;
+  color: white;
+  padding: 8px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+} 
+
+.btn:hover {
+  background-color: #c9302c;
+}
+</style>
