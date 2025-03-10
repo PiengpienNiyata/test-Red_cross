0c0f9ac3f5d505975f0268507a94419c385d2b2b
Piengpien Niyata <piengpien.nyt@gmail.com>
2025-03-10 10:19:03 +0700

    Update result.vue

--- my-vue-app/src/components/result.vue
+++ my-vue-app/src/components/result.vue
@@ -6,9 +6,36 @@ const emit = defineEmits(["save"]);
 </script>
 
 <template>
-  <div class="final-result">
-    <h4>แบบสอบถามเสร็จสมบูรณ์</h4>
-    <p>เส้นทางที่ได้รับ: {{ props.route }}</p>
-    <button @click="emit('save')">บันทึกผลลัพธ์</button>
+  <div class="final-result" >
+    <p class="p">Result Road Map: <span :style="{ color: '#EB4648' }">{{ props.route }}</span></p>
+    <button class="btn" @click="emit('save') ">บันทึกผลลัพธ์</button>
   </div>
 </template>
+
+<style>
+.final-result {
+  margin-top: 64px;
+}
+
+.p {
+  font-size: 20px;
+  padding: 8px;
+}
+
+.btn {
+  margin-left: 2px;
+  margin-top: 68px;
+  height: 100%;
+  width: 188px;
+  background-color: #EB4648;
+  color: white;
+  padding: 8px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background-color: #c9302c;
+}
+</style>
